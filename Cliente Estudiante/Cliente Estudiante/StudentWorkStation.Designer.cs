eb7562f4af5b7f6ddb85a4cdb88b37d2e6852c51
Djsiclait <djidjellypas92@gmail.com>
2017-06-22 15:48:29 -0400

    Create StudentWorkStation form

---
+++ Cliente Estudiante/Cliente Estudiante/StudentWorkStation.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Cliente_Estudiante
+{
+    partial class StudentWorkStation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menu_workstation_cliente_estudiante = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.miPerfilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.misPremiosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaDeTrabajosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaDeTrabajosToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.trabajosCompletatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.volverAlInicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trabajosCorregidosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menu_workstation_cliente_estudiante.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(103, 88);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menu_workstation_cliente_estudiante
+            // 
+            this.menu_workstation_cliente_estudiante.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.opciónToolStripMenuItem});
+            this.menu_workstation_cliente_estudiante.Location = new System.Drawing.Point(0, 0);
+            this.menu_workstation_cliente_estudiante.Name = "menu_workstation_cliente_estudiante";
+            this.menu_workstation_cliente_estudiante.Size = new System.Drawing.Size(663, 24);
+            this.menu_workstation_cliente_estudiante.TabIndex = 1;
+            this.menu_workstation_cliente_estudiante.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miPerfilToolStripMenuItem,
+            this.misPremiosToolStripMenuItem,
+            this.listaDeTrabajosToolStripMenuItem,
+            this.volverAlInicioToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // opciónToolStripMenuItem
+            // 
+            this.opciónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ayudaToolStripMenuItem});
+            this.opciónToolStripMenuItem.Name = "opciónToolStripMenuItem";
+            this.opciónToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.opciónToolStripMenuItem.Text = "Opción";
+            // 
+            // miPerfilToolStripMenuItem
+            // 
+            this.miPerfilToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listaDeTrabajosToolStripMenuItem1,
+            this.trabajosCompletatosToolStripMenuItem,
+            this.trabajosCorregidosToolStripMenuItem});
+            this.miPerfilToolStripMenuItem.Name = "miPerfilToolStripMenuItem";
+            this.miPerfilToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.miPerfilToolStripMenuItem.Text = "Trabajos";
+            // 
+            // misPremiosToolStripMenuItem
+            // 
+            this.misPremiosToolStripMenuItem.Name = "misPremiosToolStripMenuItem";
+            this.misPremiosToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.misPremiosToolStripMenuItem.Text = "Mis Premios";
+            // 
+            // listaDeTrabajosToolStripMenuItem
+            // 
+            this.listaDeTrabajosToolStripMenuItem.Name = "listaDeTrabajosToolStripMenuItem";
+            this.listaDeTrabajosToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.listaDeTrabajosToolStripMenuItem.Text = "Mi Perfil";
+            // 
+            // listaDeTrabajosToolStripMenuItem1
+            // 
+            this.listaDeTrabajosToolStripMenuItem1.Name = "listaDeTrabajosToolStripMenuItem1";
+            this.listaDeTrabajosToolStripMenuItem1.Size = new System.Drawing.Size(189, 22);
+            this.listaDeTrabajosToolStripMenuItem1.Text = "Lista de Trabajos";
+            // 
+            // trabajosCompletatosToolStripMenuItem
+            // 
+            this.trabajosCompletatosToolStripMenuItem.Name = "trabajosCompletatosToolStripMenuItem";
+            this.trabajosCompletatosToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.trabajosCompletatosToolStripMenuItem.Text = "Trabajos Completatos";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // volverAlInicioToolStripMenuItem
+            // 
+            this.volverAlInicioToolStripMenuItem.Name = "volverAlInicioToolStripMenuItem";
+            this.volverAlInicioToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.volverAlInicioToolStripMenuItem.Text = "Volver al Inicio";
+            // 
+            // trabajosCorregidosToolStripMenuItem
+            // 
+            this.trabajosCorregidosToolStripMenuItem.Name = "trabajosCorregidosToolStripMenuItem";
+            this.trabajosCorregidosToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.trabajosCorregidosToolStripMenuItem.Text = "Trabajos Corregidos";
+            // 
+            // StudentWorkStation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 326);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menu_workstation_cliente_estudiante);
+            this.MainMenuStrip = this.menu_workstation_cliente_estudiante;
+            this.Name = "StudentWorkStation";
+            this.Text = "Reading Butler WorkStation";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menu_workstation_cliente_estudiante.ResumeLayout(false);
+            this.menu_workstation_cliente_estudiante.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menu_workstation_cliente_estudiante;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem miPerfilToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listaDeTrabajosToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem trabajosCompletatosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trabajosCorregidosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem misPremiosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listaDeTrabajosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem volverAlInicioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+    }
+}
