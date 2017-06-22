8a0866c8069bf79fa5eb93926fe73fa59ed51c08
Djsiclait <djidjellypas92@gmail.com>
2017-06-22 16:37:31 -0400

    Layout StudentWorkStation windows from structure

--- Cliente Estudiante/Cliente Estudiante/StudentWorkStation.Designer.cs
+++ Cliente Estudiante/Cliente Estudiante/StudentWorkStation.Designer.cs
@@ -31,16 +31,28 @@
             this.pb_avatar_cliente_estudiante = new System.Windows.Forms.PictureBox();
             this.ms_menu_workstation_cliente_estudiante = new System.Windows.Forms.MenuStrip();
             this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.opciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.miPerfilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.misPremiosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.listaDeTrabajosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.listaDeTrabajosToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.trabajosCompletatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.volverAlInicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.trabajosCorregidosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.misPremiosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaDeTrabajosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.volverAlInicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lb_bandera_workstation_cliente_estudiante = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.lb_grupo_actual_workstation_cliente_estudiante = new System.Windows.Forms.Label();
+            this.lb_nivel_workstation_cliente_estudiante = new System.Windows.Forms.Label();
+            this.lb_estrellas_ganados_workstation_cliente_estudiante = new System.Windows.Forms.Label();
+            this.lb_premios_workstation_cliente_estudiante = new System.Windows.Forms.Label();
+            this.lv_curriculum_grupo_workstation_cliente_estudiante = new System.Windows.Forms.ListView();
+            this.lv_ejercicios_subgrupo_workstation_cliente_estudiante = new System.Windows.Forms.ListView();
+            this.lv_practicas_grupo_workstation_cliente_estudiante = new System.Windows.Forms.ListView();
+            this.lb_curriculum_grupo_workstation_cliente_estudiante = new System.Windows.Forms.Label();
+            this.lb_ejercicios_subgrupo_workstation_cliente_estudiante = new System.Windows.Forms.Label();
+            this.lb_practicas_grupo_workstation_cliente_estudiante = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pb_avatar_cliente_estudiante)).BeginInit();
             this.ms_menu_workstation_cliente_estudiante.SuspendLayout();
             this.SuspendLayout();
@@ -61,7 +73,7 @@
             this.opciónToolStripMenuItem});
             this.ms_menu_workstation_cliente_estudiante.Location = new System.Drawing.Point(0, 0);
             this.ms_menu_workstation_cliente_estudiante.Name = "ms_menu_workstation_cliente_estudiante";
-            this.ms_menu_workstation_cliente_estudiante.Size = new System.Drawing.Size(693, 24);
+            this.ms_menu_workstation_cliente_estudiante.Size = new System.Drawing.Size(679, 24);
             this.ms_menu_workstation_cliente_estudiante.TabIndex = 1;
             this.ms_menu_workstation_cliente_estudiante.Text = "menuStrip1";
             // 
@@ -76,14 +88,6 @@
             this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
             this.menuToolStripMenuItem.Text = "Menu";
             // 
-            // opciónToolStripMenuItem
-            // 
-            this.opciónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.ayudaToolStripMenuItem});
-            this.opciónToolStripMenuItem.Name = "opciónToolStripMenuItem";
-            this.opciónToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
-            this.opciónToolStripMenuItem.Text = "Opción";
-            // 
             // miPerfilToolStripMenuItem
             // 
             this.miPerfilToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
@@ -91,20 +95,8 @@
             this.trabajosCompletatosToolStripMenuItem,
             this.trabajosCorregidosToolStripMenuItem});
             this.miPerfilToolStripMenuItem.Name = "miPerfilToolStripMenuItem";
-            this.miPerfilToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.miPerfilToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
             this.miPerfilToolStripMenuItem.Text = "Trabajos";
-            // 
-            // misPremiosToolStripMenuItem
-            // 
-            this.misPremiosToolStripMenuItem.Name = "misPremiosToolStripMenuItem";
-            this.misPremiosToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.misPremiosToolStripMenuItem.Text = "Mis Premios";
-            // 
-            // listaDeTrabajosToolStripMenuItem
-            // 
-            this.listaDeTrabajosToolStripMenuItem.Name = "listaDeTrabajosToolStripMenuItem";
-            this.listaDeTrabajosToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.listaDeTrabajosToolStripMenuItem.Text = "Mi Perfil";
             // 
             // listaDeTrabajosToolStripMenuItem1
             // 
@@ -118,33 +110,169 @@
             this.trabajosCompletatosToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
             this.trabajosCompletatosToolStripMenuItem.Text = "Trabajos Completatos";
             // 
-            // ayudaToolStripMenuItem
-            // 
-            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
-            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.ayudaToolStripMenuItem.Text = "Ayuda";
-            // 
-            // volverAlInicioToolStripMenuItem
-            // 
-            this.volverAlInicioToolStripMenuItem.Name = "volverAlInicioToolStripMenuItem";
-            this.volverAlInicioToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.volverAlInicioToolStripMenuItem.Text = "Volver al Inicio";
-            // 
             // trabajosCorregidosToolStripMenuItem
             // 
             this.trabajosCorregidosToolStripMenuItem.Name = "trabajosCorregidosToolStripMenuItem";
             this.trabajosCorregidosToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
             this.trabajosCorregidosToolStripMenuItem.Text = "Trabajos Corregidos";
             // 
+            // misPremiosToolStripMenuItem
+            // 
+            this.misPremiosToolStripMenuItem.Name = "misPremiosToolStripMenuItem";
+            this.misPremiosToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.misPremiosToolStripMenuItem.Text = "Mis Premios";
+            // 
+            // listaDeTrabajosToolStripMenuItem
+            // 
+            this.listaDeTrabajosToolStripMenuItem.Name = "listaDeTrabajosToolStripMenuItem";
+            this.listaDeTrabajosToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.listaDeTrabajosToolStripMenuItem.Text = "Mi Perfil";
+            // 
+            // volverAlInicioToolStripMenuItem
+            // 
+            this.volverAlInicioToolStripMenuItem.Name = "volverAlInicioToolStripMenuItem";
+            this.volverAlInicioToolStripMenuItem.Size = new System.Drawing.Size(150, 22);
+            this.volverAlInicioToolStripMenuItem.Text = "Volver al Inicio";
+            // 
+            // opciónToolStripMenuItem
+            // 
+            this.opciónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ayudaToolStripMenuItem});
+            this.opciónToolStripMenuItem.Name = "opciónToolStripMenuItem";
+            this.opciónToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.opciónToolStripMenuItem.Text = "Opción";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // lb_bandera_workstation_cliente_estudiante
+            // 
+            this.lb_bandera_workstation_cliente_estudiante.AutoSize = true;
+            this.lb_bandera_workstation_cliente_estudiante.Location = new System.Drawing.Point(166, 28);
+            this.lb_bandera_workstation_cliente_estudiante.Name = "lb_bandera_workstation_cliente_estudiante";
+            this.lb_bandera_workstation_cliente_estudiante.Size = new System.Drawing.Size(133, 13);
+            this.lb_bandera_workstation_cliente_estudiante.TabIndex = 2;
+            this.lb_bandera_workstation_cliente_estudiante.Text = "Bienvenido johnSmith1234";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(286, 50);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 3;
+            // 
+            // lb_grupo_actual_workstation_cliente_estudiante
+            // 
+            this.lb_grupo_actual_workstation_cliente_estudiante.AutoSize = true;
+            this.lb_grupo_actual_workstation_cliente_estudiante.Location = new System.Drawing.Point(12, 179);
+            this.lb_grupo_actual_workstation_cliente_estudiante.Name = "lb_grupo_actual_workstation_cliente_estudiante";
+            this.lb_grupo_actual_workstation_cliente_estudiante.Size = new System.Drawing.Size(121, 13);
+            this.lb_grupo_actual_workstation_cliente_estudiante.TabIndex = 4;
+            this.lb_grupo_actual_workstation_cliente_estudiante.Text = "Conectado a: Bomberos";
+            // 
+            // lb_nivel_workstation_cliente_estudiante
+            // 
+            this.lb_nivel_workstation_cliente_estudiante.AutoSize = true;
+            this.lb_nivel_workstation_cliente_estudiante.Location = new System.Drawing.Point(12, 192);
+            this.lb_nivel_workstation_cliente_estudiante.Name = "lb_nivel_workstation_cliente_estudiante";
+            this.lb_nivel_workstation_cliente_estudiante.Size = new System.Drawing.Size(43, 13);
+            this.lb_nivel_workstation_cliente_estudiante.TabIndex = 5;
+            this.lb_nivel_workstation_cliente_estudiante.Text = "Nivel: 5";
+            // 
+            // lb_estrellas_ganados_workstation_cliente_estudiante
+            // 
+            this.lb_estrellas_ganados_workstation_cliente_estudiante.AutoSize = true;
+            this.lb_estrellas_ganados_workstation_cliente_estudiante.Location = new System.Drawing.Point(12, 205);
+            this.lb_estrellas_ganados_workstation_cliente_estudiante.Name = "lb_estrellas_ganados_workstation_cliente_estudiante";
+            this.lb_estrellas_ganados_workstation_cliente_estudiante.Size = new System.Drawing.Size(130, 13);
+            this.lb_estrellas_ganados_workstation_cliente_estudiante.TabIndex = 6;
+            this.lb_estrellas_ganados_workstation_cliente_estudiante.Text = "Estrellas Ganadas: 501pts";
+            // 
+            // lb_premios_workstation_cliente_estudiante
+            // 
+            this.lb_premios_workstation_cliente_estudiante.AutoSize = true;
+            this.lb_premios_workstation_cliente_estudiante.Location = new System.Drawing.Point(12, 218);
+            this.lb_premios_workstation_cliente_estudiante.Name = "lb_premios_workstation_cliente_estudiante";
+            this.lb_premios_workstation_cliente_estudiante.Size = new System.Drawing.Size(108, 13);
+            this.lb_premios_workstation_cliente_estudiante.TabIndex = 7;
+            this.lb_premios_workstation_cliente_estudiante.Text = "Premios Ganados: 12";
+            // 
+            // lv_curriculum_grupo_workstation_cliente_estudiante
+            // 
+            this.lv_curriculum_grupo_workstation_cliente_estudiante.Location = new System.Drawing.Point(16, 271);
+            this.lv_curriculum_grupo_workstation_cliente_estudiante.Name = "lv_curriculum_grupo_workstation_cliente_estudiante";
+            this.lv_curriculum_grupo_workstation_cliente_estudiante.Size = new System.Drawing.Size(215, 157);
+            this.lv_curriculum_grupo_workstation_cliente_estudiante.TabIndex = 8;
+            this.lv_curriculum_grupo_workstation_cliente_estudiante.UseCompatibleStateImageBehavior = false;
+            // 
+            // lv_ejercicios_subgrupo_workstation_cliente_estudiante
+            // 
+            this.lv_ejercicios_subgrupo_workstation_cliente_estudiante.Location = new System.Drawing.Point(237, 271);
+            this.lv_ejercicios_subgrupo_workstation_cliente_estudiante.Name = "lv_ejercicios_subgrupo_workstation_cliente_estudiante";
+            this.lv_ejercicios_subgrupo_workstation_cliente_estudiante.Size = new System.Drawing.Size(222, 157);
+            this.lv_ejercicios_subgrupo_workstation_cliente_estudiante.TabIndex = 9;
+            this.lv_ejercicios_subgrupo_workstation_cliente_estudiante.UseCompatibleStateImageBehavior = false;
+            // 
+            // lv_practicas_grupo_workstation_cliente_estudiante
+            // 
+            this.lv_practicas_grupo_workstation_cliente_estudiante.Location = new System.Drawing.Point(465, 271);
+            this.lv_practicas_grupo_workstation_cliente_estudiante.Name = "lv_practicas_grupo_workstation_cliente_estudiante";
+            this.lv_practicas_grupo_workstation_cliente_estudiante.Size = new System.Drawing.Size(203, 157);
+            this.lv_practicas_grupo_workstation_cliente_estudiante.TabIndex = 10;
+            this.lv_practicas_grupo_workstation_cliente_estudiante.UseCompatibleStateImageBehavior = false;
+            // 
+            // lb_curriculum_grupo_workstation_cliente_estudiante
+            // 
+            this.lb_curriculum_grupo_workstation_cliente_estudiante.AutoSize = true;
+            this.lb_curriculum_grupo_workstation_cliente_estudiante.Location = new System.Drawing.Point(19, 255);
+            this.lb_curriculum_grupo_workstation_cliente_estudiante.Name = "lb_curriculum_grupo_workstation_cliente_estudiante";
+            this.lb_curriculum_grupo_workstation_cliente_estudiante.Size = new System.Drawing.Size(101, 13);
+            this.lb_curriculum_grupo_workstation_cliente_estudiante.TabIndex = 11;
+            this.lb_curriculum_grupo_workstation_cliente_estudiante.Text = "Curiculum Bonberos";
+            // 
+            // lb_ejercicios_subgrupo_workstation_cliente_estudiante
+            // 
+            this.lb_ejercicios_subgrupo_workstation_cliente_estudiante.AutoSize = true;
+            this.lb_ejercicios_subgrupo_workstation_cliente_estudiante.Location = new System.Drawing.Point(245, 255);
+            this.lb_ejercicios_subgrupo_workstation_cliente_estudiante.Name = "lb_ejercicios_subgrupo_workstation_cliente_estudiante";
+            this.lb_ejercicios_subgrupo_workstation_cliente_estudiante.Size = new System.Drawing.Size(104, 13);
+            this.lb_ejercicios_subgrupo_workstation_cliente_estudiante.TabIndex = 12;
+            this.lb_ejercicios_subgrupo_workstation_cliente_estudiante.Text = "Ejercicios Asignados";
+            // 
+            // lb_practicas_grupo_workstation_cliente_estudiante
+            // 
+            this.lb_practicas_grupo_workstation_cliente_estudiante.AutoSize = true;
+            this.lb_practicas_grupo_workstation_cliente_estudiante.Location = new System.Drawing.Point(474, 255);
+            this.lb_practicas_grupo_workstation_cliente_estudiante.Name = "lb_practicas_grupo_workstation_cliente_estudiante";
+            this.lb_practicas_grupo_workstation_cliente_estudiante.Size = new System.Drawing.Size(51, 13);
+            this.lb_practicas_grupo_workstation_cliente_estudiante.TabIndex = 13;
+            this.lb_practicas_grupo_workstation_cliente_estudiante.Text = "Practicas";
+            // 
             // StudentWorkStation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.ClientSize = new System.Drawing.Size(693, 364);
+            this.ClientSize = new System.Drawing.Size(679, 439);
+            this.Controls.Add(this.lb_practicas_grupo_workstation_cliente_estudiante);
+            this.Controls.Add(this.lb_ejercicios_subgrupo_workstation_cliente_estudiante);
+            this.Controls.Add(this.lb_curriculum_grupo_workstation_cliente_estudiante);
+            this.Controls.Add(this.lv_practicas_grupo_workstation_cliente_estudiante);
+            this.Controls.Add(this.lv_ejercicios_subgrupo_workstation_cliente_estudiante);
+            this.Controls.Add(this.lv_curriculum_grupo_workstation_cliente_estudiante);
+            this.Controls.Add(this.lb_premios_workstation_cliente_estudiante);
+            this.Controls.Add(this.lb_estrellas_ganados_workstation_cliente_estudiante);
+            this.Controls.Add(this.lb_nivel_workstation_cliente_estudiante);
+            this.Controls.Add(this.lb_grupo_actual_workstation_cliente_estudiante);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.lb_bandera_workstation_cliente_estudiante);
             this.Controls.Add(this.pb_avatar_cliente_estudiante);
             this.Controls.Add(this.ms_menu_workstation_cliente_estudiante);
             this.MainMenuStrip = this.ms_menu_workstation_cliente_estudiante;
+            this.MaximumSize = new System.Drawing.Size(695, 478);
+            this.MinimumSize = new System.Drawing.Size(695, 478);
             this.Name = "StudentWorkStation";
             this.Text = "Reading Butler WorkStation";
             ((System.ComponentModel.ISupportInitialize)(this.pb_avatar_cliente_estudiante)).EndInit();
@@ -170,5 +298,17 @@
         private System.Windows.Forms.ToolStripMenuItem opciónToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lb_bandera_workstation_cliente_estudiante;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label lb_grupo_actual_workstation_cliente_estudiante;
+        private System.Windows.Forms.Label lb_nivel_workstation_cliente_estudiante;
+        private System.Windows.Forms.Label lb_estrellas_ganados_workstation_cliente_estudiante;
+        private System.Windows.Forms.Label lb_premios_workstation_cliente_estudiante;
+        private System.Windows.Forms.ListView lv_curriculum_grupo_workstation_cliente_estudiante;
+        private System.Windows.Forms.ListView lv_ejercicios_subgrupo_workstation_cliente_estudiante;
+        private System.Windows.Forms.ListView lv_practicas_grupo_workstation_cliente_estudiante;
+        private System.Windows.Forms.Label lb_curriculum_grupo_workstation_cliente_estudiante;
+        private System.Windows.Forms.Label lb_ejercicios_subgrupo_workstation_cliente_estudiante;
+        private System.Windows.Forms.Label lb_practicas_grupo_workstation_cliente_estudiante;
     }
 }
