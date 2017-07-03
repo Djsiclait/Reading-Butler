c21c18c1929c9a122120d67f9a7be03f93b301a3
Djsiclait <djidjellypas92@gmail.com>
2017-07-03 19:19:39 -0400

    Add login initiation protocol

--- JavaFXStudentClient/src/Student/Controllers/LoginController.java
+++ JavaFXStudentClient/src/Student/Controllers/LoginController.java
@@ -1,7 +1,50 @@
 package Student.Controllers;
 
+import Student.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.util.prefs.Preferences;
+
 /**
  * Created by Djidjelly Siclait on 7/1/2017.
  */
 public class LoginController {
+
+    // Main App
+    private Main mainApp;
+
+    // FXMLS
+    @FXML
+    TextField username_textfield;
+    @FXML
+    PasswordField password_passwordField;
+    @FXML
+    Button login_button;
+
+    // Configuration ans Setup Functions
+    public void setMainApp(Main mainApp){
+        this.mainApp = mainApp;
+    }
+
+    public void initialize(){
+        Preferences.systemNodeForPackage(LoginController.class);
+    }
+
+
+    // Functions
+    public void authenticateUser(){
+        System.out.println(username_textfield.getText());
+        System.out.println(password_passwordField.getText());
+    }
+
+    public void summonRegisterPage(){
+        System.out.println("register");
+    }
+
+    public void initiatePasswordRetrievalProtocol(){
+        System.out.println("Resetting password");
+    }
 }
--- JavaFXStudentClient/src/Student/Main.java
+++ JavaFXStudentClient/src/Student/Main.java
@@ -1,29 +1,72 @@
 package Student;
 
+import Student.Controllers.LoginController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.LoadException;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
 
 /**
  * Created by Djidjelly Siclait on 7/1/2017.
  */
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-//        Stage primaryStage = null;
-        Parent root = FXMLLoader.load(getClass().getResource("resources/UI/login.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.setResizable(false);
-        primaryStage.setMaximized(false);
-        primaryStage.show();
-    }
+    // Primary Stage
+    private Stage primaryStage;
 
-
+    // START HERE
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public Stage getPrimaryStage(){
+        return this.primaryStage;
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        this.primaryStage = primaryStage;
+
+        // Opening Login View
+        loginLayout();
+    }
+
+    // Client View Layouts
+    public void loginLayout(){
+        primaryStage.setTitle("Reading Butler");
+
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("resources/UI/login.fxml"));
+
+            primaryStage.setScene(new Scene((AnchorPane) loader.load()));
+
+            LoginController loginController = loader.getController();
+            loginController.setMainApp(this);
+
+            primaryStage.setResizable(false);
+            primaryStage.setMaximized(false);
+
+            primaryStage.show();
+
+        } catch (MalformedURLException e){
+            // TODO: Create Error scenarios
+            System.out.println(e.getMessage());
+        } catch (LoadException e){
+            // TODO: Create Error scenarios
+            System.out.println(e.getMessage());
+        } catch (IOException e){
+            // TODO: Create Error scenarios
+            System.out.println(e.getMessage());
+        } catch (Exception e){
+            // TODO: Create Error scenarios
+            System.out.println(e.getMessage());
+        }
     }
 }
